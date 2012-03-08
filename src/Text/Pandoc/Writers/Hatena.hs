f9bacebe61446b5dd8987bcfea54ee46939d4fe7
Jun FURUSE <jun.furuse@gmail.com>
2012-03-08 18:20:12 +0800

    priorities are noted

--- src/Text/Pandoc/Writers/Hatena.hs
+++ src/Text/Pandoc/Writers/Hatena.hs
@@ -62,21 +62,21 @@ inline (Strong is)      = wrapHTML "b"      Nothing $ inlines is
 inline (Strikeout is)   = wrapHTML "strike" Nothing $ inlines is
 inline (Superscript is) = wrapHTML "sup"    Nothing $ inlines is
 inline (Subscript is)   = wrapHTML "sub"    Nothing $ inlines is
--- CR jfuruse todo
-inline (SmallCaps is) = inlines is
-inline (Quoted quoteType is) = inlines is
-inline (Code attr str) = wrapHTML "code" Nothing $ text str
-inline (LineBreak) = text "<br/>"
-inline (Note blks) = "((" <> blocks blks <> "))" -- don't know if complex thing is inside
-inline (Cite cites is) = text ("CITE " ++ show cites) <> inlines is
-inline (Math mathType str) = text ("MATH " ++ show mathType) <> text str
-inline (RawInline fmt str) = text ("RAWINLINE " ++ show fmt) <> text str
+inline (Code _attr str) = wrapHTML "code"   Nothing $ text str
+inline (LineBreak)      = text "<br/>"
+inline (Note blks)      = "((" <> blocks blks <> "))" -- don't know if complex thing is inside
 inline (Image is (url, _title) )=
     text $ "<img" ++ alt ++ " src=\"" ++ url ++ "\"/>"
     where
         alt = case is of
             [] -> ""
             _ -> " alt=\"" ++ show is ++ "\""
+-- CR jfuruse todo. But lower priority
+inline (SmallCaps is) = inlines is
+inline (Quoted quoteType is) = inlines is
+inline (Cite cites is) = text ("CITE " ++ show cites) <> inlines is
+inline (Math mathType str) = text ("MATH " ++ show mathType) <> text str
+inline (RawInline fmt str) = text ("RAWINLINE " ++ show fmt) <> text str
 
 -- The first Para in the blocks are not prefixed by a blankline.
 blocks :: [Block] -> Doc
@@ -95,10 +95,6 @@ block (BlockQuote blks) = wrapBlock ">>" "<<" $ blocks blks
 block (BulletList blockLists) = 
     wrapBlock "<ul>" "</ul>"
     $ vcat (map (\x -> "<li>" <> space <> nest 2 x <> space <> "</li>") $ map blocks blockLists)
-
--- block (OrderedList attribs blockLists) = 
--- --   "OrderedList" <> space <> text (show attribs) $$
--- --   (prettyList $ map (prettyList . map block) blockLists)
 
 block (CodeBlock attr str) = 
     wrapInline "<!-- " " -->" (text $ show attr)
@@ -124,35 +120,21 @@ block HorizontalRule = cr <> text "<hr>" <> cr
 
 -- TODOs
 block blk = text $ show blk
--- block (OrderedList attribs blockLists) = 
---   "OrderedList" <> space <> text (show attribs) $$
---   (prettyList $ map (prettyList . map block) blockLists)
 
--- block (DefinitionList items) = "DefinitionList" $$
---   (prettyList $ map deflistitem items)
---     where deflistitem (term, defs) = "(" <> text (show term) <> "," <> cr <>
---            nest 1 (prettyList $ map (prettyList . map block) defs) <> ")"
--- block (Table caption aligns widths header rows) = 
---   "Table " <> text (show caption) <> " " <> text (show aligns) <> " " <>
---   text (show widths) $$
---   prettyRow header $$
---   prettyList (map prettyRow rows)
---     where prettyRow cols = prettyList (map (prettyList . map block) cols)
--- block (RawBlock fmt str) =
+-- High priority
 --    | OrderedList ListAttributes [[Block]] -- ^ Ordered list (attributes
 --                            -- and a list of items, each a list of blocks)
 --    | DefinitionList [([Inline],[[Block]])]  -- ^ Definition list
 --                            -- Each list item is a pair consisting of a
 --                            -- term (a list of inlines) and one or more
 --                            -- definitions (each a list of blocks)
---    | Header Int [Inline]   -- ^ Header - level (integer) and text (inlines)
---    | HorizontalRule        -- ^ Horizontal rule
+
+-- Lower priority
 --    | Table [Inline] [Alignment] [Double] [TableCell] [[TableCell]]  -- ^ Table,
 --                            -- with caption, column alignments,
 --                            -- relative column widths (0 = default),
 --                            -- column headers (each a list of blocks), and
 --                            -- rows (each a list of lists of blocks)
---    | Null                  -- ^ Nothing
 
 -- | Prettyprint Pandoc document.
 writeHatena :: WriterOptions -> Pandoc -> String
